7cbb6d5a9b40db3a5c7007f295710ed8ab480223
Robin Martinu <martinu@porg.cz>
2020-01-08 23:27:28 +0100

    added login and some styling to the page
      TODO: login api not working, post + return?

--- api-users.js
+++ api-users.js
@@ -1,9 +1,11 @@
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
+const crypto = require("crypto");
+const idLength = 4;
 
 const url = require('url');
 
-let users = new Array();
+let users = [];
 
 exports.apiUser = function (req, res) {
     let q = url.parse(req.url, true);
@@ -34,10 +36,63 @@ exports.apiUser = function (req, res) {
                 });
                 let obj = {};
                 obj.username = entities.encode(body.name);
+
                 obj.email = entities.encode(body.email);
                 obj.password = entities.encode(body.password);
-                obj.time = new Date();
+                // obj.time = new Date();
+                let id = crypto.randomBytes(idLength/2).toString("hex");
+                obj.id = id;
+
                 users.push(obj);
+
+                // users[obj.username] = obj;
+                res.end(JSON.stringify(obj));
+            }
+        })
+
+    }  else if (q.pathname == "/user/login") {
+        let data = "";
+
+        req.on('data', function (chunk) {
+            try {
+                data += chunk;
+            } catch (e) {
+                console.error(e);
+            }
+        });
+        req.on('end', function () {
+            req.rawBody = data;
+            if (data) {
+                let body = JSON.parse(data);
+                res.writeHead(200, {
+                    "Content-type": "application/json",
+
+                });
+                let obj = {};
+
+                let loginName = entities.encode(body.name);
+                let loginPass = entities.encode(body.password);
+
+                for (let i; i < users.length; i++){
+                    if (users[i].username === loginName && users[i].password === loginPass ){
+                        obj.credentialsCorrect = true;
+                        obj.name = entities.decode(users[loginName].name);
+                        break;
+                    }
+                    obj.credentialsCorrect = false;
+                }
+
+                /*
+               if (users[loginName] && users[loginName].password === loginPass){
+                   obj.credentialsCorrect = true;
+                   obj.name = entities.decode(users[loginName].name);
+               } else
+                 {
+                   obj.credentialsCorrect = false;
+                 }
+
+                 */
+
                 res.end(JSON.stringify(obj));
             }
         })
--- index.js
+++ index.js
@@ -1,13 +1,63 @@
+let user = {};
+user.logged = false;
+
+let isLogin = false;
+let isRegister = false;
+
 function showLogin(){
-document.getElementById("divLogin").style.display = "block";
+    if (isLogin){
+        document.getElementById("divLogin").style.display = "none";
+        isLogin = false;
+    } else {
+        document.getElementById("divLogin").style.display = "block";
+        isLogin = true;
+    }
+}
+
+function login(){
+ let name = document.getElementById("logName").value;
+ let pass = document.getElementById("logPass").value;
+
+ let body = {};
+    body.name = name;
+    body.password = pass;
+    let opt = {};
+    opt.method = "POST";
+    opt.body = JSON.stringify(body);
+
+    fetch(HOST+"/user/login", opt).then(function(response) {
+        response.text().then(function(text) {
+            // nemusim delat nic
+            // uz musi i POST
+            let obj = JSON.parse(text);
+
+            if (obj.credentialsCorrect) {
+                user.logged = true;
+                user.name = obj.name;
+
+                document.getElementById("divLogin").style.display = "none";
+                document.getElementById("btnLogout").style.display = "inline";
+            } else {
+                alert ("Nesprávné jméno, heslo nebo nezaregistrovaný uživatel.");
+            }
+
+        });
+    });
+
 }
 
 function logout(){
-
+    location.reload();
 }
 
 function showRegister(){
-    document.getElementById("divRegister").style.display = "block";
+    if (isRegister){
+        document.getElementById("divRegister").style.display = "none";
+        isRegister = false;
+    } else {
+        document.getElementById("divRegister").style.display = "block";
+        isRegister = true;
+    }
 }
 
 function register(){
@@ -31,8 +81,22 @@ function register(){
         fetch(HOST+"/user/register", opt).then(function(response) {
             response.text().then(function(text) {
                 // nemusim delat nic
+                    // uz musi i POST
+
+                let obj = JSON.parse(text);
+                user.id = obj.id;
+                user.logged = true;
+                user.name = name;
+
+                document.getElementById("divRegister").style.display = "none";
+                document.getElementById("btnLogout").style.display = "inline";
+
             });
         });
+
+
+
+
     } else {
         alert("Hesla se neshodují!");
         document.getElementById("regPass1").value = "";
